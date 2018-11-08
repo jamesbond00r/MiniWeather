ebe986ec3b272ac1eeec69cede1836c0bc3ba5fa
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-11-08 12:08:22 -0600

    add navbar

--- app/components/app.js
+++ app/components/app.js
@@ -1,15 +1,54 @@
 var React = require('react');
 var ReactRouter = require('react-router-dom');
 
+
+
 class App extends React.Component {
 	render(){
 		return(
-			<div className='homePage'>
-			<h1 className='title'>Enter a City and State</h1>
-			<input className='inputBox' type="text" placeholder='City, State' name="city" />
-			<button className="grnBtn">Get Weather</button>
+			<div>
+			<Header />
+			<Weather name="Enter a City and State" />
 			</div>
 			)
+	}
+}
+
+class Header extends React.Component{
+	render(){
+		return(
+		<div className="headerCon">
+		<h2 className='title'> Mini Weather </h2>
+		<div  className="header">
+
+		<Input />
+		</div>
+		</div>
+		)
+	}
+}
+
+class Weather extends React.Component{
+	render(){
+		return(
+			<div className='homePage'>
+			<h1 className='title'>{this.props.name}</h1>
+			<Input />
+			</div>
+
+		)
+	}
+}
+
+class Input extends React.Component{
+	render(){
+		return(
+			<div>
+			<input className='inputBox' type="text" placeholder='City, State' name="city" />
+			<button className="grnBtn" name="city">Get Weather</button>
+			</div>
+
+		)
 	}
 }
 
