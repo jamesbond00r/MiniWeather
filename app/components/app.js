fe4230b226cc773e933d938942afb5a388e1ed93
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-11-10 08:32:31 -0600

    Display Api results in app

--- app/components/app.js
+++ app/components/app.js
@@ -1,77 +1,103 @@
 var React = require('react');
 var ReactRouter = require('react-router-dom');
-var api = require('../utils/api.js');
-
+//var api = require('../utils/api.js');
 var axios = require('axios');
 
-const key = '0d29fbaa06dcff2656b75d959ff27d9f';
 
-
-
-
-
-
-function fetchWeather (zipCode){
-var encodedURI = window.encodeURI("http://api.openweathermap.org/data/2.5/weather?zip=67214,us&APPID=0d29fbaa06dcff2656b75d959ff27d9f");
-//http://api.openweathermap.org/data/2.5/weather?q=CITY-NAME&type=accurate&APPID=YOUR-API-KEY
-	
-return axios.get(encodedURI)
-.then(function (response){
-	console.log(response);
-	  });
-	}
-;
-
-fetchWeather(67214);
 
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+ 	
+		this.state = {
+			data:[],
+			isLoading: false,
+			zipCode: 67214,
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+
+	}
+
+	handleChange(event) {
+		this.setState({zipCode: event.target.value})
+		
+	}
+
+	handleSubmit(event) {
+		console.log(
+			this.state.zipCode);
+
+		event.preventDefault();
+	}
+
+
+
+
+	
+
+
+	componentDidMount() {
+		this.setState({isLoading: true});
+
+		const key = '0d29fbaa06dcff2656b75d959ff27d9f';
+var encodedURI = window.encodeURI("http://api.openweathermap.org/data/2.5/weather?zip="+ this.state.zipCode +",us&units=imperial&APPID=" + key);
+
+
+		axios.get(encodedURI)
+		.then(result => this.setState({
+			data: result.data.main.temp,
+			isLoading: false
+		}))
+	
+}
+
+
+
+	
 	render(){
+		const {data, isLoading} = this.state;
+
+		if (isLoading) {
+			return <p>Loading ...</p>;
+		}
 		return(
 			<div>
-			<Header />
-			<Weather name="Enter a City and State" />
+				<div className="headerCon">
+					<h2 className='title'> Mini Weather </h2>
+					<div  className="header">
+						<div> 
+							<form onSubmit={this.handleSubmit}>
+        						<label>
+         						 <input className='inputBox' type="text" placeholder='City, State' value={this.state.zipCode} onChange={this.handleChange} />
+        						</label>
+        					<input className="grnBtn" type="submit" value="Get Weather" />
+      							</form>
+						</div>
+					</div>
+				</div>
+			
+			<div className='homePage'>
+				<h1 className='title'>Enter a City and State</h1>
+			<div>
+				<form onSubmit={this.handleSubmit}>
+        			<label>
+          			<input className='inputBox' type="text" placeholder='City, State' value={this.state.zipCode} onChange={this.handleChange} />
+        			</label>
+        			<input className="grnBtn" type="submit" value="Get Weather" />
+      			</form>
+			</div>
+			<p>{this.state.data}</p>
+
+			</div>
 			</div>
 			)
 	}
 }
 
-class Header extends React.Component{
-	render(){
-		return(
-		<div className="headerCon">
-		<h2 className='title'> Mini Weather </h2>
-		<div  className="header">
-
-		<Input />
-		</div>
-		</div>
-		)
-	}
-}
-
-class Weather extends React.Component{
-	render(){
-		return(
-			<div className='homePage'>
-			<h1 className='title'>{this.props.name}</h1>
-			<Input />
-			</div>
-
-		)
-	}
-}
-
-class Input extends React.Component{
-	render(){
-		return(
-			<div>
-			<input className='inputBox' type="text" placeholder='City, State' name="city" />
-			<button className="grnBtn" name="city">Get Weather</button>
-			</div>
-
-		)
-	}
-}
+			
+			
 
 module.exports = App;
+
